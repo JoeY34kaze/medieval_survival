8368c07ec7128dc6d36fdae5de0df717f3bafb55
Joey34kaze <ragnar1724@gmail.com>
2019-06-12 18:54:47 +0200

    manually prekopirov prejsnji commit ker je git prizadet

---
+++ Assets/_scripts/Interactible_ArmorStand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactible_ArmorStand : Interactable
+{
+    public NetworkArmorStand nas;
+    public int collider_number;
+    internal override void interact(uint server_id)//sprozi na playerju
+    {
+        //server_id nimamo kaj rabit zdjle
+        Debug.Log("Interactible armor stand : " + collider_number);
+        nas.local_interaction_request(this.collider_number, server_id);
+
+    }
+}
