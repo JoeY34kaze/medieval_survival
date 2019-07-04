484cb38bed33ef68e343b80d39e7e51e11a6fd6c
Joey34kaze <ragnar1724@gmail.com>
2019-07-04 16:12:38 +0200

    scena postavljena

---
+++ Assets/UmaDNA_GuildRegistrar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UMA.CharacterSystem;
+using UnityEngine;
+
+public class UmaDNA_GuildRegistrar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DynamicCharacterAvatar avatar = GetComponent<DynamicCharacterAvatar>();
+        Dictionary<string, DnaSetter> dna = avatar.GetDNA();
+
+
+        dna["breastSize"].Set(1f);
+        dna["headSize"].Set(0f);
+
+        avatar.BuildCharacter();
+    }
+
+}
