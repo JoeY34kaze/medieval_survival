29a47fa2a059dc063bc73cd298770e84b29d9c98
Joey34kaze <ragnar1724@gmail.com>
2020-04-28 01:00:16 +0200

    start character creationa

--- Assets/Interactable_trebuchet_shot.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Interactable_trebuchet_shot : Interactable
-{
-
-}
---
+++ Assets/_scripts/character_creator_camera_handler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_creator_camera_handler : MonoBehaviour
+{
+    public Transform player_position;
+    void Update()
+    {
+        
+    }
+}
